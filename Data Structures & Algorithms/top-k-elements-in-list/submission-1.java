821b5cc4eec86e461a908e34f8d16fc28a914a05
i-vanov <93471125+i-vanov@users.noreply.github.com>
2026-08-17 14:58:57 +0300

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> frequency = new HashMap<>();
+
+        // Count the frequencies of the nums
+        for (int i : nums) {
+            frequency.put(i, frequency.getOrDefault(i, 0) + 1);
+        }
+        // 
+        Queue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
+
+        // Whenever heap size exceeds k, remove the least frequent element
+        for (Map.Entry<Integer, Integer> entry : frequency.entrySet()) {
+            pq.offer(new int[]{entry.getKey(), entry.getValue()});
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+        int[] res = new int[k];
+        int index = 0;
+
+        while(!pq.isEmpty()) {
+            int[] element = pq.poll();
+            res[index] =  element[0];
+            index++;
+        }
+        return res;
+    }
+}
