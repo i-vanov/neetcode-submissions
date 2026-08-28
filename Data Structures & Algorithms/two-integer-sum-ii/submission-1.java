bcce7cfac1f5dd8f9030fe85ef739d8c1cc7c5cd
i-vanov <93471125+i-vanov@users.noreply.github.com>
2026-08-28 19:48:36 +0300

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[] {left + 1, right + 1};
+            }
+            // Move right pointer inward since the sum is bigger
+            // than the target and the array is sorted
+            if (sum > target) {
+                right--;
+            }
+            // Move left pointer inward since the sum is smaller
+            // than the target and the array is sorted
+            else {
+                left++;
+            }
+        }
+        throw new IllegalStateException("No solution found");
+    }
+}
