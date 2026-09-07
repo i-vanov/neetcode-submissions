d22a95cf66755b8ff72bd0696d2e0203964e73d5
i-vanov <93471125+i-vanov@users.noreply.github.com>
2026-09-07 16:24:39 +0300

    Add: longest-substring-without-duplicates - submission-5

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-5.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int maxLength = 0;
+        Set<Character> charSet = new HashSet<>();
+        char[] chars = s.toCharArray();
+        int i = 0;
+        int j = 0;
+
+        if (s.length() < 2) {
+            return s.length();
+        }
+        while (j < chars.length) {
+            // Shrink the substring from the left until the duplicate is gone
+            while (charSet.contains(chars[j])) {
+                charSet.remove(chars[i]);
+                i++;
+            }
+            charSet.add(chars[j]);
+            maxLength = Math.max(maxLength, j - i + 1);
+            j++;
+        }
+        return maxLength;
+    }
+}
